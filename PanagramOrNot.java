9803edfc2cd50a0538660a5532aad072ecbb0a8d
Bartłomiej Szkodny <szkobart@outlook.com>
2023-04-09 19:35:59 +0200

    panagram checker algorithm
    Program that checks if sentence is panagram

---
+++ PanagramOrNot.java
@@ -0,0 +1,20 @@
+public class PanagramOrNot {
+    public static void main(String[] args) {
+        String s = "The quick brown fox jumps over a lazy dog.";
+        System.out.println(isPanagram(s.toLowerCase()));
+
+    }
+
+    private static boolean isPanagram(String s) {
+        if (s.length() < 26) {
+            return false;
+        } else {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                if (s.indexOf(ch) < 0) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
