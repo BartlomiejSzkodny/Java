590093b2568c363652b77e0f7ea636b6ddd51181
Bartłomiej Szkodny <szkobart@outlook.com>
2023-05-02 10:59:33 +0200

    Work in progess

---
+++ (W)ReplaceAllDigitswithCharacters.java
@@ -0,0 +1,17 @@
+import java.lang.reflect.Array;
+
+public class ReplaceAllDigitswithCharacters {
+    public static void main(String[] args) {
+        String s = "a1c1e1";
+        replaceDigits(s);
+    }
+
+    public static String replaceDigits(String s) {
+        char[] array=s.toCharArray();
+        for(int i=0;i<s.length();i++){
+            if(array[i]==1)
+        }
+
+    }
+
+}
